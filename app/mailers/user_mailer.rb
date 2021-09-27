1966915b52e7338e72314c1acabe7397d1796ec3
thanh <thanh.lq011299@gmail.com>
2021-09-27 07:50:43 +0700

    Chapter 12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: t("activation.subject")
   end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("reset_password.title")
+  end
 end
